919e494d9d9bd1da7cc6612dcbab9373898bfb85
nehakaur1 <nehakaur816@gmail.com>
2025-08-06 22:57:35 +0530

    update

--- UseEffect/src/App.jsx
+++ UseEffect/src/App.jsx
@@ -1,29 +1,42 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-  function neha(){
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-    .then(res=>res.json())
-    .then(data=>console.log(data.setup+'_'+data.punchline)
-      
-    )
+  const [count, setCount] = useState(0);
+  //useEffect
+  async function neha() {
+    const res = await fetch(
+      "https://official-joke-api.appspot.com/jokes/random"
+    );
+    const data = await res.json();
+    console.log(data.setup + "_" + data.punchline);
   }
-  useEffect(()=>{
+  useEffect(() => {
     neha();
-  },[])
+  }, []);
 
+  //useRef
+  // to understand plz comment out useeffect
+  // const value=useRef(0);
+  //  function counting(){
+  //   value.current=value.current+1
+  //  }
+  //  counting()
   return (
     <>
-    <p>{count}</p>
-      <button onClick={()=>{
-setCount(count+1)
-      }}>add</button>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        add
+      </button>
+      <p>This component has rendered {value.current} times</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
